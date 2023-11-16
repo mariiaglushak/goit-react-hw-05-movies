fecc1a65d99b85845e056e1d09733926f51399f5
mariiaglushak <90639229+mariiaglushak@users.noreply.github.com>
2023-11-16 23:22:47 +0200

    дом

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import { FetchRequest } from 'Services/request';
 import { DefaultImage } from 'components/DefaulImage/ingex';
@@ -8,8 +8,16 @@ import { Name, ListCast, ItemCast, MainName, Character } from './CastStyle';
 const Cast = () => {
   const [castDetails, setCastDetails] = useState(null);
   const { movieId } = useParams();
+  const messageRef = useRef();
 
   useEffect(() => {
+    if (messageRef.current) {
+      messageRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+        inline: 'nearest',
+      });
+    }
     const requestCast = async () => {
       const URL_CAST = `/movie/${movieId}/credits`;
       try {
@@ -24,31 +32,33 @@ const Cast = () => {
   }, [movieId]);
 
   return (
-    <ListCast>
-      {castDetails?.length === 0 ? (
-        <p>We don't have cast of this movie </p>
-      ) : (
-        castDetails?.map(el => (
-          <ItemCast key={el.name}>
-            <img
-              src={
-                el.profile_path
-                  ? `https://image.tmdb.org/t/p/w300${el.profile_path}`
-                  : DefaultImage
-              }
-              width="200px"
-              alt={el.name}
-            />
-            <MainName>{el.name}</MainName>
-            {el.character && (
-              <Character>
-                Character:<Name>{el.character}</Name>
-              </Character>
-            )}
-          </ItemCast>
-        ))
-      )}
-    </ListCast>
+    <div ref={messageRef}>
+      <ListCast>
+        {castDetails?.length === 0 ? (
+          <p>We don't have cast of this movie </p>
+        ) : (
+          castDetails?.map(el => (
+            <ItemCast key={el.name}>
+              <img
+                src={
+                  el.profile_path
+                    ? `https://image.tmdb.org/t/p/w300${el.profile_path}`
+                    : DefaultImage
+                }
+                width="200px"
+                alt={el.name}
+              />
+              <MainName>{el.name}</MainName>
+              {el.character && (
+                <Character>
+                  Character:<Name>{el.character}</Name>
+                </Character>
+              )}
+            </ItemCast>
+          ))
+        )}
+      </ListCast>
+    </div>
   );
 };
 
